95d4ba03129bb33e9d8290b8f5b6cf0b21350f9d
Thomas J Bradley <hey@thomasjbradley.ca>
2016-01-16 17:42:40 -0500

    Upgrade commit counting system

--- .tests/submit-grade.js
+++ .tests/submit-grade.js
@@ -1,24 +1,20 @@
 var
-  readPkg = require('read-pkg'),
+  pkg = require('../package.json'),
   gitExecute = require('git-execute'),
   path = require('path'),
   util = require('util'),
   https = require('https'),
-  repoPath = path.resolve(process.env.REPO || ('./.git'))
+  repoPath = path.resolve(process.env.REPO || ('./.git')),
+  proxyUrl = 'https://travis-canvas-proxy.appspot.com/grade?%s'
 ;
 
 if (!process.env.TRAVIS_PULL_REQUEST) return;
 
-var
-  proxyUrl = 'https://travis-canvas-proxy.appspot.com/grade?%s',
-  pkg = readPkg.sync()
-;
-
 var getVars = [
   util.format('gh_repo=%s', encodeURI(process.env.TRAVIS_REPO_SLUG)),
   util.format('gh_pr=%s', process.env.TRAVIS_PULL_REQUEST),
-  util.format('canvas_course=%s', pkg['canvas-course']),
-  util.format('canvas_assignment=%s', pkg.name)
+  util.format('canvas_course=%s', pkg.autoMarks.canvasCourse),
+  util.format('canvas_assignment=%s', pkg.autoMarks.canvasAssignment)
 ];
 
 https.get(util.format(proxyUrl, getVars.join('&')), function (res) {
--- .tests/test-2-git-commits.js
+++ .tests/test-2-git-commits.js
@@ -1,12 +1,11 @@
 var
+  pkg = require('../package.json'),
   MultiLineError = require('./multi-line-error').MultiLineError,
   fs = require('fs'),
   path = require('path'),
   util = require('util'),
   gitCountCommits = require('git-count-commits')
 ;
-
-var MIN_COMMITS = 14;
 
 /*
   +++++++++++++++++++++++++++++++++++++++++++++++
@@ -19,12 +18,17 @@ describe('# git commits', function () {
   it('has enough commits', function (done) {
     var repoPath = path.resolve(process.env.REPO || ('./.git'));
 
-    gitCountCommits(repoPath, function(err, commits) {
-      if (commits < MIN_COMMITS || err) {
-        throw new MultiLineError('Git Commits', [util.format("Not enough commits to the repository (has %d, expecting %d)", commits, MIN_COMMITS)]);
-      }
+    gitCountCommits(repoPath, 'gh-pages', function(err, originalCommits) {
+      gitCountCommits(repoPath, function(e, allCommits) {
+        // -1 from allCommits to accommodate the Travis CI merge commit
+        var commitsDiff = (allCommits - 1) - originalCommits;
 
-      done();
+        if (commitsDiff < pkg.autoMarks.commits) {
+          throw new MultiLineError('Git Commits', [util.format("Not enough commits to the repository (has %d, expecting %d)", commitsDiff, pkg.autoMarks.commits)]);
+        }
+
+        done();
+      });
     });
   });
 
