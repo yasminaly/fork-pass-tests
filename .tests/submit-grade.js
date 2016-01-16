2c7aa84647ecc8aa3a22ef624e1af5fa6e1a3ada
Thomas J Bradley <hey@thomasjbradley.ca>
2016-01-16 15:51:51 -0500

    Fix grade submission system

--- .tests/submit-grade.js
+++ .tests/submit-grade.js
@@ -2,32 +2,25 @@ var
   readPkg = require('read-pkg'),
   gitExecute = require('git-execute'),
   path = require('path'),
+  util = require('util'),
   https = require('https'),
   repoPath = path.resolve(process.env.REPO || ('./.git'))
 ;
 
 if (!process.env.TRAVIS_PULL_REQUEST) return;
 
-// process.env.TRAVIS_COMMIT;
-// process.env.TRAVIS_REPO_SLUG;
+var
+  proxyUrl = 'https://travis-canvas-proxy.appspot.com/grade?%s',
+  pkg = readPkg.sync()
+;
 
-gitExecute(repoPath, ['rev-parse', '--abbrev-ref', 'HEAD'], function(err, stdout, stderr) {
-  var
-    ghUsername = stdout.trim().replace(/-gh-pages/, ''),
-    proxyUrl = 'https://travis-canvas-proxy.appspot.com/grade?%s'
-  ;
+var getVars = [
+  util.format('gh_repo=%s', encodeURI(process.env.TRAVIS_REPO_SLUG)),
+  util.format('gh_pr=%s', process.env.TRAVIS_PULL_REQUEST),
+  util.format('canvas_course=%s', pkg['canvas-course']),
+  util.format('canvas_assignment=%s', pkg.name)
+];
 
-  readPkg().then(function (pkg) {
-    var getVars = [
-      util.format('gh_user=%s', encodeURI(ghUsername)),
-      util.format('gh_repo=%s', encodeURI(process.env.TRAVIS_REPO_SLUG)),
-      util.format('gh_pr=%s', process.env.TRAVIS_PULL_REQUEST),
-      util.format('canvas_course=%s', pkg['canvas-course']),
-      util.format('canvas_assignment=%s', pkg.name)
-    ];
-
-    https.get(util.format(proxyUrl, getVars.join('&'), function (res) {});
-  });
-
-  console.log(ghUsername);
-});
+https.get(util.format(proxyUrl, getVars.join('&')), function (res) {
+  res.on('data', function (data) { });
+}).on('error', function (e) { });
